9335b06a1a46e9976191c452cf4389fdddb3c0ec
frozn45 <ag.twitch@exalounge.de>
2023-01-28 11:39:32 +0100

    - fix: scaling issue to prevent cascading scales

--- Libraries/LibDropdown-1.0.lua
+++ Libraries/LibDropdown-1.0.lua
@@ -92,7 +92,7 @@ end
 new, del = lib.new, lib.del
 
 -- Make the frame match the tooltip
-local function InitializeFrame(frame, parent)
+local function InitializeFrame(frame)
 	if TooltipBackdropTemplateMixin then
 		frame.layoutType = GameTooltip.layoutType
 		if GameTooltip.layoutType then
@@ -109,9 +109,9 @@ local function InitializeFrame(frame, parent)
 			frame:SetBackdropBorderColor(GameTooltip:GetBackdropBorderColor())
 		end
 	end
-	
-	if not parent then
-		frame:SetScale(GameTooltip:GetScale())
+	frame:SetScale(GameTooltip:GetScale())
+	if (frame:GetEffectiveScale() ~= GameTooltip:GetEffectiveScale()) then -- consider applied SetIgnoreParentScale() on GameTooltip regarding scaling of the frame
+		frame:SetScale(frame:GetScale() * GameTooltip:GetEffectiveScale() / frame:GetEffectiveScale())
 	end
 end
 
@@ -625,7 +625,7 @@ end
 
 function AcquireFrame(parent, toplevel)
 	local f = tremove(framePool) or NewDropdownFrame()
-	InitializeFrame(f, parent) -- set the look of the frame
+	InitializeFrame(f) -- set the look of the frame
 	f.released = false
 	f.buttons = f.buttons or {}
 	f:SetParent(parent or UIParent)
